790f958cb5dd4c6aa81d222af8bb8dfcab5ee041
SaboM7 <07.09.abv@gmail.com>
2023-05-04 20:11:00 +0200

    Final version. Added comments and moved all functions to functions.py.

--- functions.py
+++ functions.py
@@ -1,14 +1,10 @@
 from urllib.request import urlopen
 from urllib.request import Request
-from pathlib import Path
-from bs4 import BeautifulSoup as bs
 import cyrtranslit
 import regex
 from datetime import datetime
-import base64
 import webbrowser
 import os
-from functions import *
 from pathlib import Path
 import tksheet
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Checkbutton, IntVar, Toplevel, Label, messagebox
@@ -16,12 +12,8 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Checkbutton, In
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Skola\Diplomski\gui\build\assets\frame0")
 
-link_list = []
-list_of_paths = []
-temp_number = 0
 
-
-def bool_to_text(value:bool):
+def bool_to_text(value: bool):
     """
     Turning boolean values into text for display.
     :param value: Value to display
@@ -45,7 +37,7 @@ def check_page(url: str, words: str, temp_number: int, path_list: list):
     :param words: string that you search
     :param temp_number: number of temp file
     :param path_list: list of paths to save path on
-    :return: Result if string is found and if the page has marked words and if page has Cyrilic characters
+    :return: Result if string is found and if the page has marked words and if page has Cyrillic characters
     """
     match = True
     # link_list.append(url)                                               #??
@@ -53,13 +45,13 @@ def check_page(url: str, words: str, temp_number: int, path_list: list):
     html_contents = urlopen(req).read()
     string_byte_decoded = html_contents.decode("UTF-8")                 # decoding html page
 
-    if regex.search(r'\p{IsCyrillic}', words) is not None :
+    if regex.search(r'\p{IsCyrillic}', words) is not None:
         temp_words = cyrtranslit.to_latin(words)                            # convert words to latin if cyr
     else:
         temp_words = cyrtranslit.to_cyrillic(words)                         # convert words to cyr if latin
     cyr_flag = regex.search(r'\p{IsCyrillic}', string_byte_decoded) is not None
 
-    if string_byte_decoded.lower().find(" "+words.lower()) >= 0 :     # checking if words exist in text
+    if string_byte_decoded.lower().find(" "+words.lower()) >= 0:     # checking if words exist in text
         marked = encoding_saving_marking(string_byte_decoded, words, path_list, temp_number)
         return match, marked, cyr_flag
     elif string_byte_decoded.lower().find(" "+temp_words.lower()) >= 0:
@@ -89,14 +81,15 @@ def encoding_saving_marking(string_to_encode_: str, words: str, path_list: list,
     path_list.append(save_to_temp(html_contents, temp_number))  # saving html page to temp and saving path
     return marked
 
+
 def open_file_update_list(list_of_links: list):
     """
     Updates list of links to search from.
     :param list_of_links: List to update.
     :return: Nothing.
     """
+    path = Path(__file__).parent / "links.txt"
     try:
-        path = Path(__file__).parent / "links.txt"
         open(path, "x")                                     # creating file if it doesnt exist
     except FileExistsError:
         pass
@@ -113,8 +106,8 @@ def save_links(list_of_links):
     :param list_of_links: List that gets saved to file.
     :return: Nothing.
     """
+    path = Path(__file__).parent / "links.txt"
     try:
-        path = Path(__file__).parent / "links.txt"
         open(path, "x")                         # creating txt file to save links to
     except FileExistsError:
         pass
@@ -122,9 +115,247 @@ def save_links(list_of_links):
         f = open(path, "w")
         for link in list_of_links:
             if not link.endswith("\n"):
-                link = link +"\n"
+                link = link + "\n"
             f.write(link)
         f.close()
+
+
+def check_if_selected_chbox(link_dict: dict, selected: dict):
+    """
+    Checking which checkboxes are selected
+    :param link_dict: Dictionary of links and their titles
+    :param selected: Dictionary to check which button is selected
+    :return: List of links to search from
+    """
+    list_to_search = []
+    for paper in link_dict.keys():
+        if selected[paper].get():                          # if checkbox is selected
+            list_to_search.append(link_dict[paper])
+    return list_to_search
+
+
+def load_links(entry):
+    """
+    Loads links from text file into the textfield
+    :param entry: Entry (textfield) to put links in
+    :return: None
+    """
+    link_list_for_textarea = []
+    open_file_update_list(link_list_for_textarea)                   # function to put links from file to list
+    for index, link in enumerate(link_list_for_textarea, 1):        # putting links in textfield
+        entry.insert(str(index) + ".0", link)
+
+
+def get_links_from_textfield(entry, list_of_links: list):
+    """
+    Getting lines from textfield and putting them in list
+    :param entry: Textfield to load lines from
+    :param list_of_links: List to put the links in
+    :return: None
+    """
+    for link in entry.get("1.0", "end").splitlines():               # getting whole text in textfield and then splitting it in lines
+        if link != "" and link not in list_of_links:
+            list_of_links.append(link)
+
+
+def search_sites(link_dict: dict, selected: dict, textfield, textbox, main_window):
+    """
+    Function that searches the words from textbox in the links from textbox or selected checkboxes
+    :return: None
+    """
+    list_to_check = check_if_selected_chbox(link_dict, selected)              # making a list of links to check from checkboxes
+    get_links_from_textfield(textfield, list_to_check)                         # adding links from textfield to list to check
+    words = textbox.get()                                                   # getting the words to search for
+    if words != "" and len(list_to_check) > 0:                               # if there is a word/s and is a link start searching
+        checking_links_making_window(list_to_check, words, main_window)
+
+
+def checking_links_making_window(list_to_check: list, words: str, main_window):
+    """
+    Searches and marks words if found in the links provided in the list
+    :param list_to_check: List of links from which to search for words
+    :param words: Words to search for
+    :param main_window: Main window to attach the new window with results
+    :return: None
+    """
+    start_time = getting_time()
+    # initializing lists to be used
+    list_of_paths = []                                                # list to store paths from downloaded pages
+    list_of_finds = []                                                # list to store if search is successful
+    list_of_marks = []                                                # list to store if the searched word is marked in downloaded page
+    list_of_Cyrilic = []                                              # list to store if the page has some Cyrillic characters
+    list_of_lists = [list_to_check, list_of_finds, list_of_marks, list_of_Cyrilic]  # list of all lists that is used to make a sheet
+
+    for i, link in enumerate(list_to_check, 0):
+        found, marked, isCyrilic = check_page(link, words, i, list_of_paths)            # checking each page
+        # adding values to lists
+        list_of_finds.append(bool_to_text(found))
+        list_of_marks.append(bool_to_text(marked))
+        list_of_Cyrilic.append(bool_to_text(isCyrilic))
+
+    # making new window to display results of search
+    new_window = Toplevel(
+        main_window,
+        bg="#444642",
+        height=511,
+        width=1058,
+        bd=0,
+        highlightthickness=0,
+        relief="ridge"
+    )
+    new_window.title("Rezultat")
+    sheet = tksheet.Sheet(new_window, width=580)
+    sheet.set_sheet_data([[item[index] for item in list_of_lists] for index in range(len(list_to_check))])  # putting values from lists into the sheet row by row
+    sheet.column_width(column=0, width=200)
+    sheet.column_width(column=1, width=100)
+    sheet.column_width(column=2, width=100)
+    sheet.column_width(column=3, width=130)
+    sheet.headers(["Linkovi", "Postoji tekst", "Označen tekst", "Stranica ima ćirilicu"])
+    sheet.enable_bindings(
+        (
+            "single_select",
+            "row_select",
+            "column_width_resize",
+            "arrowkeys",
+            "right_click_popup_menu",
+            "rc_select",
+            "copy",
+            "cut",
+            "paste"
+        )
+    )
+    sheet.highlight_columns(1, "yellow", "red")                     # highlighting the column with the result if words are found
+    sheet.place(
+        x=48.0,
+        y=100.0,
+        height=45 + len(list_to_check) * 24.0,
+        width=580.0
+    )
+    add_predefined_labels(new_window, words, start_time)             # adding labels that are necessary to window
+    add_buttons_to_window(list_of_paths, new_window, 640.0)         # adding buttons for opening downloaded pages
+    add_buttons_to_window(list_to_check, new_window, 820.0)         # adding buttons for opening pages on web
+
+
+def add_buttons_to_window(list_to_make_buttons: list, master_window, x_position: float):
+    """
+    Makes buttons and places them to window from a list of links
+    :param list_to_make_buttons: List of links from which buttons are made
+    :param master_window:
+    :param x_position: X to place the buttons to
+    :return: None
+    """
+    for index, link in enumerate(list_to_make_buttons, 0):
+        button_temp = Button(
+            text="Otvori",
+            master=master_window,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda link=link: webbrowser.open(link),         # command is opening link in default web browser
+            relief="flat"
+        )
+        button_temp.place(
+            x=x_position,
+            y=125.0 + index * 23.0,                                 # placing the button one below other
+            width=100.0,
+            height=20.0
+        )
+
+
+def add_predefined_labels(new_window, words: str, start_time: str):
+    """
+    Adds labels to window
+    :param new_window:
+    :param words:
+    :param start_time: Time that searching started to put in label
+    :return:
+    """
+    l1 = Label(
+        new_window,
+        text='Traženi tekst: ' + words,
+        bd=0,
+        anchor="w",
+        font=("Inter Light", 25 * -1),
+        bg='#454642',
+        fg='yellow',
+        activebackground='#454642',
+        activeforeground='white'
+    )
+    l1.place(
+        x=70.0,
+        y=30.0,
+    )
+    l1 = Label(
+        new_window,
+        text='Otvori skinutu stranicu: ',
+        bd=0,
+        anchor="w",
+        font=("Inter Light", 15 * -1),
+        bg='#454642',
+        fg='orange',
+        activebackground='#454642',
+        activeforeground='white'
+    )
+    l1.place(
+        x=640.0,
+        y=70.0,
+    )
+    l1 = Label(
+        new_window,
+        text='Otvori stranicu sa interneta: ',
+        bd=0,
+        anchor="w",
+        font=("Inter Light", 15 * -1),
+        bg='#454642',
+        fg='green',
+        activebackground='#454642',
+        activeforeground='white'
+    )
+    l1.place(
+        x=820.0,
+        y=70.0,
+    )
+    l1 = Label(
+        new_window,
+        text='Pretraženo ' + start_time,
+        bd=0,
+        anchor="w",
+        font=("Inter Light", 15 * -1),
+        bg='#454642',
+        fg='white',
+        activebackground='#454642',
+        activeforeground='white'
+    )
+    l1.place(
+        x=640.0,
+        y=20.0,
+    )
+
+
+def making_checkboxes(link_dict: dict, selected: dict):
+    """
+    Makes checkboxes from dict of links and putting values in selected dict
+    :param link_dict: Dictionary of links and their titles
+    :param selected: Dictionary to put the selected values in
+    :return: None
+    """
+    for index, paper in enumerate(link_dict.keys()):
+        selected[paper] = IntVar()                      # making variable to store checkbutton value
+        check = Checkbutton(
+            text=paper,                                 # setting title of link to be display on checkbutton
+            variable=selected[paper],                   # setting the variable to be value of title key
+            bd=0,
+            anchor="w",
+            font=("Inter Light", 20 * -1),
+            bg='#454642',
+            fg='white',
+            activebackground='#454642',
+            activeforeground='white',
+            selectcolor="#454642")
+        check.place(
+            x=70.0,
+            y=210.0 + index * 30.0,
+            width=100.0,
+            height=30.0)
 
 
 def getting_time():
@@ -145,8 +376,8 @@ def save_to_temp(html_contents: bytes, temp_num):
     :param temp_num: Number of temp file.
     :return: Path to them file that page is saved to.
     """
+    path = Path(__file__).parent
     try:
-        path = Path(__file__).parent
         os.mkdir(path/"tmp")                                     # making temp directory if it doesnt exist
     except FileExistsError:
         pass
@@ -159,7 +390,7 @@ def save_to_temp(html_contents: bytes, temp_num):
         return file_path
 
 
-def mark_string (string_for_marking: str, words: str):
+def mark_string(string_for_marking: str, words: str):
     """
     Marking string in html page.
     :param string_for_marking:
@@ -173,26 +404,15 @@ def mark_string (string_for_marking: str, words: str):
     return string_for_return
 
 
-# print(getting_time())
-# open_file_update_list(link_list)
-# for i,link in enumerate(link_list,0) :
-#     a,b,c= check_page(link,"vučević",len(list_of_paths),list_of_paths)
-#     print(f"{a} {b} {c}")
-# for path in list_of_paths :
-#     webbrowser.open(path)
-#
-# save_links(link_list)
-
-# req = Request("https://www.politika.rs/sr", headers={'User-Agent' : "Magic Browser"})
-# html_contents = urlopen( req )
-# html_contents = urlopen("https://www.politika.rs/sr").read()
-# with open("test.html", 'wb') as f:  # opening file
-#     f.write(html_contents)
-# webbrowser.open("test.html")
-
-# a, b, c = check_page(url, "вучевић", 0, list_of_paths)
-# a, b, c = check_page(url2, "vučević", 1, list_of_paths)
-# a, b, c = check_page(url1, "vučević", 2, list_of_paths)
-#
-# webbrowser.open(list_of_paths[0])
-# webbrowser.open(list_of_paths[1])
+def on_closing(window, textfield):
+    """
+    Function that is called on trying to close the main window
+    :param window: Window to be closed
+    :param textfield: Text field to take links from
+    :return: None
+    """
+    if messagebox.askokcancel("Izlaz", "Da li želite da izadjete?"):  # asking are you sure to quit
+        list_to_save = []
+        get_links_from_textfield(textfield, list_to_save)                # loading links from text filed to save
+        save_links(list_to_save)                                      # saving links to text file
+        window.destroy()                                              # closing window
--- gui/build/gui.py
+++ gui/build/gui.py
@@ -3,35 +3,26 @@
 # https://github.com/ParthJadhav/Tkinter-Designer
 
 from functions import *
-# from pathlib import Path
-#
-# # from tkinter import *
-# # Explicit imports to satisfy Flake8
-# import tksheet
-# from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Checkbutton, IntVar, Toplevel, Label
 
 
-# OUTPUT_PATH = Path(__file__).parent
-# ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Skola\Diplomski\gui\build\assets\frame0")
-
-
-
+# making base window
 window = Tk()
 window.title("Pretraži stranice")
 window.geometry("1058x511")
-window.configure(bg = "#444642")
+window.configure(bg="#444642")
 
+# adding canvas
 canvas = Canvas(
     window,
-    bg = "#444642",
-    height = 511,
-    width = 1058,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
+    bg="#444642",
+    height=511,
+    width=1058,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge"
 )
 
-canvas.place(x = 0, y = 0)
+canvas.place(x=0, y=0)
 canvas.create_text(
     300.0,
     31.0,
@@ -41,6 +32,7 @@ canvas.create_text(
     font=("Inter Black", 32 * -1)
 )
 
+# adding image for text box for searched word
 entry_image_1 = PhotoImage(
     file=relative_to_assets("entry_1.png"))
 entry_bg_1 = canvas.create_image(
@@ -54,7 +46,7 @@ entry_1 = Entry(
     bg="#D9D9D9",
     fg="#000716",
     highlightthickness=0,
-    font = ("Inter Light", 20 * -1)
+    font=("Inter Light", 20 * -1)
 )
 entry_1.place(
     x=73.0,
@@ -62,6 +54,7 @@ entry_1.place(
     width=284.0,
     height=32.0
 )
+# adding label
 canvas.create_text(
     70.0,
     180.0,
@@ -72,45 +65,11 @@ canvas.create_text(
 )
 
 
-def making_checkboxes(link_dict : dict,selected : dict):
-    for index, paper in enumerate(link_dict.keys()):
-        selected[paper] = IntVar()
-        l = Checkbutton(
-            text=paper,
-            variable=selected[paper],
-            bd=0,
-            anchor="w",
-            font=("Inter Light", 20 * -1),
-            bg='#454642',
-            fg='white',
-            activebackground='#454642',
-            activeforeground='white',
-            selectcolor="#454642")
-        l.place(
-            x=70.0,
-            y=210.0 + index * 30.0,
-            width=100.0,
-            height=30.0)
+link_dict = {"Blic": "https://www.blic.rs/", "Alo": "https://www.alo.rs/", "Danas": "https://www.danas.rs/",     # dict of default links with titles
+             "Nova": "https://nova.rs/", "N1": "https://n1info.rs/", "Politika": "https://www.politika.rs/scc"}
+selected = {}                                                                                                   # dict to save checkbox values
+making_checkboxes(link_dict, selected)                           # making checkboxes of default links
 
-
-link_dict = {"Blic":"https://www.blic.rs/","Alo": "https://www.alo.rs/", "Danas": "https://www.danas.rs/" ,
-             "Nova": "https://nova.rs/", "N1":"https://n1info.rs/","Politika":"https://www.politika.rs/scc"}
-selected = {}
-making_checkboxes(link_dict,selected)
-
-
-def check_if_selected_chbox(link_dict:dict,selected : dict):
-    list_to_search = []
-    for paper in link_dict.keys():
-        if selected[paper].get() :
-            list_to_search.append(link_dict[paper])
-    return list_to_search
-
-def load_links(entry):
-    link_list_for_textarea = []
-    open_file_update_list(link_list_for_textarea)
-    for index, link in enumerate(link_list_for_textarea, 1):
-        entry.insert(str(index) + ".0", link)
 
 canvas.create_text(
     73.0,
@@ -139,6 +98,7 @@ canvas.create_text(
     font=("Inter Light", 13 * -1)
 )
 
+# TextField
 entry_image_2 = PhotoImage(
     file=relative_to_assets("entry_2.png"))
 entry_bg_2 = canvas.create_image(
@@ -146,13 +106,12 @@ entry_bg_2 = canvas.create_image(
     289.0,
     image=entry_image_2
 )
-# TextField
 entry_2 = Text(
     bd=0,
     bg="#D9D9D9",
     fg="#000716",
     highlightthickness=0,
-    font = ("Inter Light", 18 * -1)
+    font=("Inter Light", 18 * -1)
 )
 
 load_links(entry_2)                         # loading links into text field
@@ -164,148 +123,16 @@ entry_2.place(
     height=228.0
 )
 
-def get_links_from_textfield(entry,list_of_links:list):
-    for link in entry.get("1.0", "end").splitlines():
-        if link != ""  and link not in list_of_links:
-            list_of_links.append(link)
-
-def search_sites():
-    list_to_check= check_if_selected_chbox(link_dict,selected)
-    get_links_from_textfield(entry_2,list_to_check)
-    # for link in entry_2.get("1.0", "end").splitlines():
-    #     if link != ""  and link not in list_to_check:
-    #         list_to_check.append(link)
-    words = entry_1.get()
-    if words != "" and len(list_to_check)>0 :
-        checking_links_making_window(list_to_check,words)
-
-
-def checking_links_making_window(list_to_check:list,words:str):
-    list_of_paths = []
-    list_of_finds = []
-    list_of_marks = []
-    list_of_Cyrilic = []
-    list_of_lists = [list_to_check, list_of_finds, list_of_marks, list_of_Cyrilic]
-    for i, link in enumerate(list_to_check, 0):
-        found, marked, isCyrilic = check_page(link, words, i, list_of_paths)
-        list_of_finds.append(bool_to_text(found))
-        list_of_marks.append(bool_to_text(marked))
-        list_of_Cyrilic.append(bool_to_text(isCyrilic))
-    new_window = Toplevel(
-        window,
-        bg="#444642",
-        height=511,
-        width=1058,
-        bd=0,
-        highlightthickness=0,
-        relief="ridge"
-    )
-    new_window.title("Rezultat")
-    sheet = tksheet.Sheet(new_window, width=580)
-    sheet.set_sheet_data([[item[index] for item in list_of_lists] for index in range(len(list_to_check))])
-    sheet.column_width(column=0, width=200)
-    sheet.column_width(column=1, width=100)
-    sheet.column_width(column=2, width=100)
-    sheet.column_width(column=3, width=130)
-    sheet.headers(["Linkovi", "Postoji tekst", "Označen tekst", "Stranica ima ćirilicu"])
-    sheet.enable_bindings(
-        (
-            "single_select",
-            "row_select",
-            "column_width_resize",
-            "arrowkeys",
-            "right_click_popup_menu",
-            "rc_select",
-            "copy",
-            "cut",
-            "paste"
-        )
-    )
-    sheet.highlight_columns(1, "yellow", "red")
-    sheet.place(
-        x=48.0,
-        y=100.0,
-        height=45 + len(list_to_check) * 24.0,
-        width=580.0
-    )
-    add_predefined_labels(new_window, words)
-    add_buttons_to_window(list_of_paths, new_window, 640.0)
-    add_buttons_to_window(list_to_check, new_window, 820.0)
-
-
-def add_buttons_to_window(list_to_make_buttons: list,master_window,x_position: float):
-    for index, link in enumerate(list_to_make_buttons,0):
-        button_temp = Button(
-            text="Otvori",
-            master=master_window,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda link=link: webbrowser.open(link),
-            relief="flat"
-        )
-        button_temp.place(
-            x=x_position,
-            y=125.0 + index * 23.0,
-            width=100.0,
-            height=20.0
-        )
-
-def add_predefined_labels(new_window,words):
-    l1 = Label(
-        new_window,
-        text='Traženi tekst: ' + words,
-        bd=0,
-        anchor="w",
-        font=("Inter Light", 25 * -1),
-        bg='#454642',
-        fg='yellow',
-        activebackground='#454642',
-        activeforeground='white'
-    )
-    l1.place(
-        x=70.0,
-        y=30.0,
-    )
-    l1 = Label(
-        new_window,
-        text='Otvori skinutu stranicu: ',
-        bd=0,
-        anchor="w",
-        font=("Inter Light", 15 * -1),
-        bg='#454642',
-        fg='orange',
-        activebackground='#454642',
-        activeforeground='white'
-    )
-    l1.place(
-        x=640.0,
-        y=70.0,
-    )
-    l1 = Label(
-        new_window,
-        text='Otvori stranicu sa interneta: ',
-        bd=0,
-        anchor="w",
-        font=("Inter Light", 15 * -1),
-        bg='#454642',
-        fg='green',
-        activebackground='#454642',
-        activeforeground='white'
-    )
-    l1.place(
-        x=820.0,
-        y=70.0,
-    )
-
 
 # Pretraži
+# button for search
 button_image_1 = PhotoImage(
     file=relative_to_assets("button_1.png"))
 button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=search_sites,                       # dodati moju funkciju za check_page
+    command=lambda: search_sites(link_dict, selected, entry_2, entry_1, window),
     relief="flat"
 )
 button_1.place(
@@ -314,14 +141,16 @@ button_1.place(
     width=137.0,
     height=42.0
 )
+
 # Učitaj linkove
+# button for loading links
 button_image_2 = PhotoImage(
     file=relative_to_assets("button_2.png"))
 button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command= lambda : load_links(entry_2),    # dugme za linkove
+    command=lambda: load_links(entry_2),    # dugme za linkove
     relief="flat"
 )
 button_2.place(
@@ -330,13 +159,7 @@ button_2.place(
     width=115.0,
     height=32.0
 )
-def on_closing(window):
-    if messagebox.askokcancel("Izlaz", "Da li želite da izadjete?"):
-        list_to_save = []
-        get_links_from_textfield(entry_2,list_to_save)
-        save_links(list_to_save)
-        window.destroy()
 
-window.protocol("WM_DELETE_WINDOW", lambda : on_closing(window))
+window.protocol("WM_DELETE_WINDOW", lambda: on_closing(window, entry_2))      # setting function when closing window
 window.resizable(False, False)
 window.mainloop()
